a2ce37fefe882600553f5ac837c40cfe81916256
thibauth01 <hermantthibaut@gmail.com>
2018-12-03 22:21:50 +0100

    Update gameController.java

--- src/mastermind/gameController.java
+++ src/mastermind/gameController.java
@@ -14,7 +14,7 @@ public class GameController  {
 	private static char miCorrect = '/';			// si le chiffre existe mais n'est pas au bon endroit
 	private static char noCorrect = 'X';			// si le chiffre n'existe pas dans la combi
 	private static char result;	
-	public static char[] combiIn = new char[4];	// pour la longueur du tableau qui réceptionne la combi qu'on encode
+	public 	static char[] combiIn = new char[4];	// pour la longueur du tableau qui rÃ©ceptionne la combi qu'on encode
 	private static String combiStr;					
 	private static int easy = 12;
 	private static int normal = 9 ;
@@ -32,17 +32,19 @@ public class GameController  {
 	
 		public static void main(String[] args){  
 
-				RandomCombi game = new RandomCombi();							// gamePC --> générer la combi
-				GameController gameCtrl = new GameController();		// gameController --> pour la correction 	IDEAL, CHANGER CELA DE CLASSE
+				RandomCombi game = new RandomCombi();							// gamePC --> gÃ©nÃ©rer la combi
+				GameController gameCtrl = new GameController();// gameController --> pour la correction 	IDEAL, CHANGER CELA DE CLASSE
+				Correction corr = new Correction();
+				
 				Scanner combiInput = new Scanner(System.in);
 				Scanner levelInput = new Scanner(System.in);
 								// pour afficher la solution
-										/*	System.out.print("solution:    "); 
+											System.out.print("solution:    "); 
 											System.out.print(game.combi[0]+" ");
 											System.out.print(game.combi[1]+" ");
 											System.out.print(game.combi[2]+" ");
 											System.out.println(game.combi[3]+"\n \n");
-										*/	//System.out.println("-------------\n");
+											//System.out.println("-------------\n");
 				
 				
 				
@@ -91,7 +93,7 @@ public class GameController  {
 					         for(int i =0; i < 4;i++) {
 					        	 combiIn[i] = combiStr.charAt(i);
 					         }
-					         //_______ écrire la combi______
+					         //_______ Ã©crire la combi______
 					          System.out.print("\nvotre combi: "); 
 						      System.out.print(combiIn[0]+" ");
 						      System.out.print(combiIn[1]+" ");
@@ -100,13 +102,13 @@ public class GameController  {
 						      System.out.println("----------------------");
 						      //_____________________________
 						      System.out.print("             ");
-							  System.out.print(gameCtrl.correction1(game)+" ");
-							  System.out.print(gameCtrl.correction2(game)+" ");
-							  System.out.print(gameCtrl.correction3(game)+" ");
-							  System.out.println(gameCtrl.correction4(game)+" \n");
+							  System.out.print(corr.correction1(combiIn,game.combi)+" ");
+							  System.out.print(corr.correction2(combiIn,game.combi)+" ");
+							  System.out.print(corr.correction3(combiIn,game.combi)+" ");
+							  System.out.println(corr.correction4(combiIn,game.combi)+" \n");
 						      j++;
-						      if ((gameCtrl.correction1(game)=='V')&& (gameCtrl.correction2(game)=='V') && (gameCtrl.correction3(game)=='V') && (gameCtrl.correction4(game)=='V')) {
-						    	  System.out.print("BRAVO \nVous avez gagné!!");
+						     if ((corr.correction1(combiIn,game.combi)=='V')&& (corr.correction2(combiIn,game.combi)=='V') && (corr.correction3(combiIn,game.combi)=='V') && (corr.correction4(combiIn,game.combi)=='V')) {
+						    	  System.out.print("BRAVO \nVous avez gagnÃ©!!");
 						    	  j=limite + 1;
 						      }
 						      else if (j==limite +1) {
@@ -116,110 +118,5 @@ public class GameController  {
 			         }
 		
 	    }
-		/*public static int convertir() {
-			
-			int[] intArray = new int[s.length()];
-
-			for (int i = 0; i < str.length(); i++) {
-				intArray[i] = Character.digit(str.charAt(i), 4);
-				
-				combi = intArray[0];
-				return combi
-				
-			}
-			
-			
-		}*/
-	    
-			// correction du CombiIn 1
-		/*public char correction69(RandomCombi game) {
-			
-			
-			for(int i = 0; i > 4;i++) {
-				
-				if (combiIn[0] == game.combi[0]) {				// premier chiffre
-		             result = correct;
-		         } else if (combiIn[0] == (char)game.combi[1]) {		// deuxieme chiffre
-		             result = miCorrect; 
-		         } else if (combiIn[0] == (char)game.combi[2]) {		// troisieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[0] == (char)game.combi[3]) {		// quatrieme chiffre
-		             result = miCorrect;
-		         } else {
-		             result = noCorrect;
-		         }	      
-				
-	         return resultTb[i]=result; 
-			}
-			return resultTb[];
-		}*/
-		
-		
-		
-		
-		
-		
-		
-		public char correction1(RandomCombi game){
-			
-			 if (combiIn[0] == game.combi[0]) {				// premier chiffre
-	             result = correct;
-	         } else if (combiIn[0] == (char)game.combi[1]) {		// deuxieme chiffre
-	             result = miCorrect; 
-	         } else if (combiIn[0] == (char)game.combi[2]) {		// troisieme chiffre
-	             result = miCorrect;
-	         } else if (combiIn[0] == (char)game.combi[3]) {		// quatrieme chiffre
-	             result = miCorrect;
-	         } else {
-	             result = noCorrect;
-	         }	         
-	         return result; 
-		}
-		  public char correction2(RandomCombi game) {
-				
-		         if (combiIn[1] == game.combi[1]) {				// premier chiffre
-		             result = correct;
-		         } else if (combiIn[1] == game.combi[0]) {		// deuxieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[1] == game.combi[2]) {		// troisieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[1] == game.combi[3]) {		// quatrieme chiffre
-		             result = miCorrect;
-		         } else {
-		             result = noCorrect;
-		         }
-		         return result;	         
-		    }
-		  public char correction3(RandomCombi game) {
-				
-		         if (combiIn[2] == game.combi[2]) {				// premier chiffre
-		             result = correct;
-		         } else if (combiIn[2] == game.combi[0]) {		// deuxieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[2] == game.combi[1]) {		// troisieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[2] == game.combi[3]) {		// quatrieme chiffre
-		             result = miCorrect;
-		         } else {
-		             result = noCorrect;
-		         }
-		         return result;	         
-		    }
-		  public char correction4(RandomCombi game) {
-				
-		         if (combiIn[3] == game.combi[3]) {				// premier chiffre
-		             result = correct;
-		         } else if (combiIn[3] == game.combi[0]) {		// deuxieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[3] == game.combi[1]) {		// troisieme chiffre
-		             result = miCorrect;
-		         } else if (combiIn[3] == game.combi[2]) {		// quatrieme chiffre
-		             result = miCorrect;
-		         } else {
-		             result = noCorrect;
-		         }
-		         return result;	         
-		    }
-					    
-	    
+				    
 }
