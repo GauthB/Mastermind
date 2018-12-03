a1116b20ae9a90dc7157bb5d66e74efa8d3449de
thibauth01 <hermantthibaut@gmail.com>
2018-12-03 22:25:11 +0100

    Update and rename gamePc.java to RandomCombi.java

---
+++ src/mastermind/RandomCombi.java
@@ -0,0 +1,42 @@
+package mastermind;
+
+import java.util.Random;
+
+public class RandomCombi {
+
+	
+	public char combi[];
+	public String combiRandom = "0123456789";
+	
+	public RandomCombi(){
+		int n=4;							// nombre de chiffre qu'il faut deviner
+	    combi=genererCombi(n);
+	}
+	
+	
+	public char[] genererCombi(int n)
+    {
+     
+		
+		Random r = new Random();
+		int SIZE = combiRandom.length();
+		char combi[] = new char [n];
+		for(int i = 0; i<4; i++) {
+			combi[i]= combiRandom.charAt(r.nextInt(SIZE));
+			
+			
+		}
+	
+	 
+     return combi;
+    }
+	
+	
+	
+	public char[] getCombi(){
+		return this.combi;
+	}
+	
+	
+	 
+}
