3e48a9288ef0058eab8022ea1d25ffed18412e33
Thibaut Hermant <hermantthibaut@gmail.com>
2018-12-09 00:00:47 +0100

    Update Correction.java

--- src/mastermind/Correction.java
+++ src/mastermind/Correction.java
@@ -2,33 +2,31 @@ package mastermind;
 
 public class Correction {
 
-	
-	
 	private static char correct = 'V';			// si le chiffre est au bon endroit
 	private static char miCorrect = '/';			// si le chiffre existe mais n'est pas au bon endroit
 	private static char noCorrect = 'X';			// si le chiffre n'existe pas dans la combi
-	private static String result = "";			// variable pour les rÃƒÂ©sultats 
+	
 	
 
-	public String correction (char[]combiIn, char[] combiRand) {
+	public char[] correction (char[]combiIn, char[] combiRand) {
 		
-
+		char[] result = new char[4];	
 		for(int i=0; i < 4; i++) {
 			
+			
 			if(combiIn[i] == combiRand[i]) {
-				result += correct;
+				result[i] += correct;
 			}
 			else if(combiIn[i] == combiRand[0] || combiIn[i] == combiRand[1] || combiIn[i] == combiRand[2] || combiIn[i] == combiRand[3]  ) {
-				result += miCorrect;
+				result[i] += miCorrect;
 			}
 			
 			else {
-				result += noCorrect;
+				result[i] += noCorrect;
 			}
 		}
 		
 		return result;
 	}
-		
-	
 }
+
