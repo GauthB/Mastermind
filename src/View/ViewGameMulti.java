9365683e33e6ac251cca51cc856804114d4513a1
GauthB <39745082+GauthB@users.noreply.github.com>
2018-12-12 10:42:40 +0100

    Update ViewGameMulti.java

--- src/View/ViewGameMulti.java
+++ src/View/ViewGameMulti.java
@@ -21,13 +21,13 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 public class ViewGameMulti extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JTextField textField1;
 	private JTextField textField2;
-	String temp="";
+	String temp="----";
 
 
 	private JLabel label_1;
@@ -35,6 +35,10 @@ public class ViewGameMulti extends JFrame implements ActionListener {
 	private JLabel label_2;
 	private JLabel label_3;
 	GameController GameController = new GameController();
+	private JTextPane combiInTout;
+	private JTextPane combiResult;
+	private JTextPane combiCompetitor;
+	private JLabel lblGameMuli;
 	
 	/**
 	 * Launch the application.
@@ -58,15 +62,15 @@ public class ViewGameMulti extends JFrame implements ActionListener {
 	 */
 	public ViewGameMulti() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 665, 589);
+		setBounds(100, 100, 630, 589);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{35, 80, 80, 80, 80, 35, 80, 80, 80, 35};
+		gbl_contentPane.columnWidths = new int[]{70, 70, 70, 70, 70, 70, 70, 70, 70};
 		gbl_contentPane.rowHeights = new int[]{29, 37, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 29, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 		contentPane.setLayout(gbl_contentPane);
 		
@@ -79,6 +83,15 @@ public class ViewGameMulti extends JFrame implements ActionListener {
 		gbc_label_3.gridx = 0;
 		gbc_label_3.gridy = 1;
 		contentPane.add(label_3, gbc_label_3);
+		
+		lblGameMuli = new JLabel("Game Multi");
+		lblGameMuli.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		GridBagConstraints gbc_lblGameMuli = new GridBagConstraints();
+		gbc_lblGameMuli.gridwidth = 10;
+		gbc_lblGameMuli.insets = new Insets(0, 0, 5, 5);
+		gbc_lblGameMuli.gridx = 0;
+		gbc_lblGameMuli.gridy = 2;
+		contentPane.add(lblGameMuli, gbc_lblGameMuli);
 		
 		label_1 = new JLabel("Your game");
 		GridBagConstraints gbc_label_1 = new GridBagConstraints();
@@ -101,24 +114,46 @@ public class ViewGameMulti extends JFrame implements ActionListener {
 		label_2.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_label_2 = new GridBagConstraints();
 		gbc_label_2.anchor = GridBagConstraints.WEST;
-		gbc_label_2.gridwidth = 3;
+		gbc_label_2.gridwidth = 2;
 		gbc_label_2.insets = new Insets(0, 0, 5, 5);
 		gbc_label_2.gridx = 6;
 		gbc_label_2.gridy = 3;
 		contentPane.add(label_2, gbc_label_2);
 		
-		textField1 = new JTextField();
-		GridBagConstraints gbc_textField1 = new GridBagConstraints();
-		gbc_textField1.gridwidth = 2;
-		gbc_textField1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField1.gridx = 1;
-		gbc_textField1.gridy = 4;
-		contentPane.add(textField1, gbc_textField1);
-		textField1.setColumns(10);
+		combiInTout = new JTextPane();
+		GridBagConstraints gbc_combiInTout = new GridBagConstraints();
+		gbc_combiInTout.gridwidth = 2;
+		gbc_combiInTout.gridheight = 11;
+		gbc_combiInTout.insets = new Insets(0, 0, 5, 5);
+		gbc_combiInTout.fill = GridBagConstraints.BOTH;
+		gbc_combiInTout.gridx = 1;
+		gbc_combiInTout.gridy = 4;
+		contentPane.add(combiInTout, gbc_combiInTout);
+		combiInTout.setEditable(false);
+		
+		combiResult = new JTextPane();
+		GridBagConstraints gbc_combiResult = new GridBagConstraints();
+		gbc_combiResult.gridheight = 11;
+		gbc_combiResult.insets = new Insets(0, 0, 5, 5);
+		gbc_combiResult.fill = GridBagConstraints.BOTH;
+		gbc_combiResult.gridx = 4;
+		gbc_combiResult.gridy = 4;
+		contentPane.add(combiResult, gbc_combiResult);
+		combiResult.setEditable(false);
+		
+		combiCompetitor = new JTextPane();
+		GridBagConstraints gbc_combiCompetitor = new GridBagConstraints();
+		gbc_combiCompetitor.gridheight = 11;
+		gbc_combiCompetitor.gridwidth = 2;
+		gbc_combiCompetitor.insets = new Insets(0, 0, 5, 5);
+		gbc_combiCompetitor.fill = GridBagConstraints.BOTH;
+		gbc_combiCompetitor.gridx = 6;
+		gbc_combiCompetitor.gridy = 4;
+		contentPane.add(combiCompetitor, gbc_combiCompetitor);
+		combiCompetitor.setEditable(false);
 		
 		
-		JButton btnSwap = new JButton("Swap");
+		JButton btnSwap = new JButton("Enter");
 		GridBagConstraints gbc_btnSwap = new GridBagConstraints();
 		gbc_btnSwap.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSwap.gridx = 0;
@@ -151,33 +186,30 @@ public class ViewGameMulti extends JFrame implements ActionListener {
 		gbc_btnExit.gridy = 18;
 		btnExit.addActionListener(this);
 		contentPane.add(btnExit, gbc_btnExit);
-		
-	
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+public void actionPerformed(ActionEvent e) {
 		
 		String textOrigine="";
 		
 		switch(e.getActionCommand()){
-		case"Swap":
+		case"Enter":
 			
 			textOrigine=textField2.getText();
 		
 			temp = temp+"\n"+textOrigine;
+			combiInTout.setText(temp);
 			
-			textField1.setText(temp);
 			textField2.setText("");
 	
 			break;
 		case"Clear":
-	
+		
 			textField2.setText("");
 			break;
 		case"Exit":
 			this.dispose();
 		}
 	}
-
 }
