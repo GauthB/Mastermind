6960b04f7ff14020098e542ca1485f24b5236de3
Thibaut Hermant <hermantthibaut@gmail.com>
2018-12-17 18:55:59 +0100

    Update ViewLevel.java

--- src/View/ViewLevel.java
+++ src/View/ViewLevel.java
@@ -1,11 +1,13 @@
-package View;
-import mastermind.GameControllerGUI;
+package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.border.EmptyBorder;
+
+import controller.GameControllerGUI;
+
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
