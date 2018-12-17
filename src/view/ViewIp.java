4ed39f3e899742359feb9e2364e5dfe77c44f135
GauthB <39745082+GauthB@users.noreply.github.com>
2018-12-17 23:24:48 +0100

    Update ViewIp.java

--- src/view/ViewIp.java
+++ src/view/ViewIp.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import controller.GameController;
+import model.ModelGame;
 
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
@@ -19,7 +19,7 @@ import java.awt.Font;
 
 public class ViewIp extends JFrame implements ActionListener {
 
-	GameController GameController = new GameController();
+	ModelGame GameController = new ModelGame();
 	/**
 	 * Variables
 	 */
