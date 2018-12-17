bb4e25a322d5f7c6f1e62e1233c911562605dd92
Thibaut Hermant <hermantthibaut@gmail.com>
2018-12-17 18:55:03 +0100

    Update ViewHote.java

--- src/View/ViewHote.java
+++ src/View/ViewHote.java
@@ -1,11 +1,12 @@
-package View;
-import mastermind.GameController;
-
+package view;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.GameController;
+
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
