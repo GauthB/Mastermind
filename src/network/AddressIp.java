7eb363c50956ebfcded5908c59b467734dba4b1e
GauthB <39745082+GauthB@users.noreply.github.com>
2018-12-19 23:18:30 +0100

    Update AddressIp.java

--- src/network/AddressIp.java
+++ src/network/AddressIp.java
@@ -3,48 +3,18 @@ package network;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-/**
- * 
- * @author Bohyn Gauthier
- * @author Hermand Thibaut
- * @author Meyers Humbert
- * Une classe qui retourne l'adressse ip locale de l'utilisateur pour le mode multi
- */
 public class AddressIp {
+public String ip;
 	
-	public InetAddress LocaleAdresse ;
-	public String adressIp;
-	public String addressIpString;
+	
 
-	
-	public Object RecupAdressIp() {
-		return adressIp;
-	}
-	
-	public String Transfo(){
-		
-		addressIpString=LocaleAdresse.toString();
-		
-		return addressIpString;
-	}
-	
-	
-	
-	/*public  void main(String[] zero) {
-		
-		
-		try {
+	   public  String giveMyIp() throws UnknownHostException {
 
-			LocaleAdresse = InetAddress.getLocalHost();
-			System.out.println("L'adresse locale est : "+LocaleAdresse ); 
-			System.out.println("---------");
-			System.out.println("l'adresse ip est: "+Transfo());
-			
-		} catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		}
-	}*/
+			ip = InetAddress.getLocalHost().getHostAddress().toString();
+			return ip;
+
+				   }
+
+
 
 }
-
