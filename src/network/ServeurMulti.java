ca833ae019b8bd01669e3f0c38d36f2f85ccc736
Thibaut Hermant <hermantthibaut@gmail.com>
2018-12-20 04:37:17 +0100

    Update ServeurMulti.java

--- src/network/ServeurMulti.java
+++ src/network/ServeurMulti.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package network;
 
 import java.io.BufferedReader;
@@ -9,22 +6,32 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+
+
+import view.ViewGameMulti;
+
 import java.io.PrintWriter;
 import java.io.IOException;
-/**
- * @author Hermant Thibaut
- * @author Humbert Meyers
- * @author Bohyn Gauhtier
- *
- */
-public class ServeurMulti  {
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextPane;
+
+
+public class ServeurMulti {
 	
-	/*public ServeurMulti() {
+	public int port;
+	
+	public ServeurMulti() throws Exception {
+		
+		ViewGameMulti viewGameMulti = new ViewGameMulti();
+		viewGameMulti.setVisible(true);
+		
 		try {
 			System.out.println("Server is started");
 			
-			ServerSocket ss = new ServerSocket(9999);
+			ServerSocket ss = new ServerSocket(port);
 			
 			System.out.println("Server is waiting for client request");
 			Socket s = ss.accept();
@@ -52,62 +59,5 @@ public class ServeurMulti  {
 			System.out.println(e);
 		}
 		
-	}*/
-	
-
-	
-	 
-	   public static void main(String[] test) {
-	  
-	     final ServerSocket serveurSocket  ;
-	     final Socket clientSocket ;
-	     final BufferedReader in;
-	     final PrintWriter out;
-	     final Scanner sc=new Scanner(System.in);
-	  
-	     try {
-	       serveurSocket = new ServerSocket(5000);
-	       clientSocket = serveurSocket.accept();
-	       out = new PrintWriter(clientSocket.getOutputStream());
-	       in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream()));
-	       Thread envoi= new Thread(new Runnable() {
-	          String msg;
-	          @Override
-	          public void run() {
-	             while(true){
-	                msg = sc.nextLine();
-	                out.println(msg);
-	                out.flush();
-	             }
-	          }
-	       });
-	       envoi.start();
-	   
-	       Thread recevoir= new Thread(new Runnable() {
-	          String msg ;
-	          @Override
-	          public void run() {
-	             try {
-	                msg = in.readLine();
-	                //tant que le client est connecté
-	                while(msg!=null){
-	                   System.out.println("Client : "+msg);
-	                   msg = in.readLine();
-	                }
-	                //sortir de la boucle si le client a déconecté
-	                System.out.println("Client déconecté");
-	                //fermer le flux et la session socket
-	                out.close();
-	                clientSocket.close();
-	                serveurSocket.close();
-	             } catch (IOException e) {
-	                  e.printStackTrace();
-	             }
-	         }
-	      });
-	      recevoir.start();
-	      }catch (IOException e) {
-	         e.printStackTrace();
-	      }
-	   }
+	}
 }
