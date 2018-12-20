63d314cf841486b35d43ede6e37b75592032716a
GauthB <39745082+GauthB@users.noreply.github.com>
2018-12-20 17:45:00 +0100

    Update RandomCombi.java

--- src/controller/RandomCombi.java
+++ src/controller/RandomCombi.java
@@ -3,17 +3,26 @@ package controller;
 import java.util.Random;
 
 public class RandomCombi {
-	
-//_______________VARIABLES___________________________________________________________________________________________________________
+
+	//_______________VARIABLES___________________________________________________________________________________________________________
 	public char combi[];
 	public String combiRandom = "0123456789";
 	
-//_________________METHODES____________________________________________________________________________________________________	
+	
+	//_________________METHODES____________________________________________________________________________________________________	
+	/**
+	 * Definit le nombre qu'il faut de chiffre dans la combi
+	 */
 	public RandomCombi(){	// nombre de chiffre qu'il faut deviner
 		int n = 4;
 	    combi=genererCombi(n);
 	}
 	
+	/**
+	 * 
+	 * @param le nombre de chiffre qu'il faut dans la combinaison
+	 * @return un combinaison aléatoire
+	 */
 	public char[] genererCombi(int n){
 		Random r = new Random();
 		int SIZE = combiRandom.length();
@@ -21,10 +30,11 @@ public class RandomCombi {
 		for(int i = 0; i<n; i++) {
 			combi[i]= combiRandom.charAt(r.nextInt(SIZE));	
 		}
-     	return combi;
-  	}
+    	return combi;
+    	}
 	
 	public char[] getCombi(){
 		return this.combi;
 	}
+	
 }
