441b85e27cd5a8c4b8c0486d6409ed16ce114f4d
Thibaut Hermant <hermantthibaut@gmail.com>
2018-12-20 01:04:02 +0100

    Update ServeurMulti.java

--- src/network/ServeurMulti.java
+++ src/network/ServeurMulti.java
@@ -9,7 +9,9 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.IOException;
 /**
  * @author Hermant Thibaut
  * @author Humbert Meyers
@@ -18,7 +20,7 @@ import java.net.Socket;
  */
 public class ServeurMulti  {
 	
-	public static void main(String[] args) throws Exception {
+	/*public ServeurMulti() {
 		try {
 			System.out.println("Server is started");
 			
@@ -47,10 +49,65 @@ public class ServeurMulti  {
 			s.close();
 		}
 		catch(Exception e) {
-			//TODO
+			System.out.println(e);
 		}
 		
+	}*/
+	
 
-	}
-
+	
+	 
+	   public static void main(String[] test) {
+	  
+	     final ServerSocket serveurSocket  ;
+	     final Socket clientSocket ;
+	     final BufferedReader in;
+	     final PrintWriter out;
+	     final Scanner sc=new Scanner(System.in);
+	  
+	     try {
+	       serveurSocket = new ServerSocket(5000);
+	       clientSocket = serveurSocket.accept();
+	       out = new PrintWriter(clientSocket.getOutputStream());
+	       in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream()));
+	       Thread envoi= new Thread(new Runnable() {
+	          String msg;
+	          @Override
+	          public void run() {
+	             while(true){
+	                msg = sc.nextLine();
+	                out.println(msg);
+	                out.flush();
+	             }
+	          }
+	       });
+	       envoi.start();
+	   
+	       Thread recevoir= new Thread(new Runnable() {
+	          String msg ;
+	          @Override
+	          public void run() {
+	             try {
+	                msg = in.readLine();
+	                //tant que le client est connecté
+	                while(msg!=null){
+	                   System.out.println("Client : "+msg);
+	                   msg = in.readLine();
+	                }
+	                //sortir de la boucle si le client a déconecté
+	                System.out.println("Client déconecté");
+	                //fermer le flux et la session socket
+	                out.close();
+	                clientSocket.close();
+	                serveurSocket.close();
+	             } catch (IOException e) {
+	                  e.printStackTrace();
+	             }
+	         }
+	      });
+	      recevoir.start();
+	      }catch (IOException e) {
+	         e.printStackTrace();
+	      }
+	   }
 }
